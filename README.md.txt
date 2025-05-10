e667fcd334e4705e2d472a14a184dbffc377ceec
guilhermern14 <55002907+guilhermern14@users.noreply.github.com>
2025-05-10 16:15:10 -0300

    Add files via upload

---
+++ README.md.txt
@@ -0,0 +1,2 @@
+# Mordomize Financeiro  
+Sistema de gestão financeira com WhatsApp.  
---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, request  
+from twilio.twiml.messaging_response import MessagingResponse  
+
+app = Flask(__name__)  
+
+@app.route("/whatsapp", methods=['POST'])  
+def whatsapp_bot():  
+    msg = request.form.get('Body', '').lower()  
+    resp = MessagingResponse()  
+    
+    if "#saldo" in msg:  
+        resp.message("Seu saldo é R$ 1000,00")  
+    elif "#ajuda" in msg:  
+        resp.message("Comandos: #saldo, #gastar [valor] [categoria]")  
+    
+    return str(resp)  
+
+if __name__ == "__main__":  
+    app.run(debug=True)  
